95b0f29ec1011f08d297c8d0df7336571f2be45a
Shadow786s <sg2333970@gmail.com>
2026-07-28 23:56:14 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Ultimate AI Translator</h1>
+
+      <p>
+        AI-powered SRT subtitle translator
+      </p>
+    </div>
+  );
+}
+
+export default App;
