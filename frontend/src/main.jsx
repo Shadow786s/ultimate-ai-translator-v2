f9999345a3a38524bdba95f0de2811b200022b2d
Shadow786s <sg2333970@gmail.com>
2026-07-29 01:22:33 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import App from "./App.jsx";
+
+import "./styles.css";
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <div
-    style={{
-      minHeight: "100vh",
-      background: "white",
-      color: "black",
-      padding: "50px",
-      fontSize: "30px",
-    }}
-  >
-    React Test Successful
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
