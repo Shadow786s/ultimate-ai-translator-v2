dd96a4eae454f99c85537a2ddffb721f1c845948
Shadow786s <sg2333970@gmail.com>
2026-07-29 15:26:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,5 @@
 function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        backgroundColor: "white",
-        color: "red",
-        padding: "50px",
-        fontSize: "40px",
-        fontWeight: "bold",
-      }}
-    >
-      APP COMPONENT WORKING
-    </div>
-  );
+  return <h1>HELLO FROM APP</h1>;
 }
 
 export default App;
