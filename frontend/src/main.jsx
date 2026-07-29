20a8f5a9d97fdd9dfdf73f51bff81437d5499b0c
Shadow786s <sg2333970@gmail.com>
2026-07-29 13:50:55 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import App from "./App.jsx";
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <div
-    style={{
-      minHeight: "100vh",
-      background: "white",
-      color: "black",
-      padding: "50px",
-      fontFamily: "Arial, sans-serif",
-      fontSize: "30px",
-    }}
-  >
-    REACT IS WORKING
-  </div>
+  <App />
 );
