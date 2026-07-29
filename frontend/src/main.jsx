da93cb3eb644fe8a5b3e24ecf268adf9f4a6a3bb
Shadow786s <sg2333970@gmail.com>
2026-07-29 15:20:25 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App.jsx";
 
-const root = ReactDOM.createRoot(
+ReactDOM.createRoot(
   document.getElementById("root")
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+).render(
+  <App />
 );
