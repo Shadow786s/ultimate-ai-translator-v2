7374f0749b3e15af5ab5c696bc9a7216ce98adc6
Shadow786s <sg2333970@gmail.com>
2026-07-29 14:02:02 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let App;
-
-try {
-  const module = await import("./App.jsx");
-  App = module.default;
-} catch (error) {
-  document.getElementById("root").innerHTML = `
-    <div style="
-      padding: 30px;
-      color: red;
-      background: white;
-      font-family: Arial, sans-serif;
-      white-space: pre-wrap;
-    ">
-      <h1>App Import Error</h1>
-      <pre>${error.stack || error.message}</pre>
-    </div>
-  `;
-
-  throw error;
-}
+import App from "./App.jsx";
 
 ReactDOM.createRoot(
   document.getElementById("root")
