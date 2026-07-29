761b44166e08c4c1c73dc46ec095f1f62801947a
Shadow786s <sg2333970@gmail.com>
2026-07-29 13:04:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-import "./styles.css";
-
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div
+    style={{
+      minHeight: "100vh",
+      background: "white",
+      color: "black",
+      padding: "50px",
+      fontSize: "30px",
+    }}
+  >
+    <div>App Import Test</div>
+
+    <div style={{ marginTop: "20px" }}>
+      {typeof App}
+    </div>
+  </div>
 );
